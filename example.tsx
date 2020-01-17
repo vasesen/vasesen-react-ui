0e8e412f8457411310a51a8be38e86a783ad4e6c
Vasesen <918811737@qq.com>
2020-01-17 22:45:15 +0800

    搭建好架子

---
+++ example.tsx
@@ -0,0 +1,11 @@
+import Icon from "./lib/components/icon/icon"
+//import Button from "./lib/components/button/button"
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+ReactDOM.render((
+    <div>
+        <Icon name="dianzan"></Icon>
+
+    </div>
+),document.querySelector('#root'))
